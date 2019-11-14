eb28c34cf1315353b630f284cd86e53758d2a362
Gabriel San Martin <gssm.snf19@uea.edu.br>
2019-11-14 18:26:10 -0400

    Classe game

--- main.py
+++ main.py
@@ -1,17 +1,47 @@
 import pygame as pg
 from utils import *
+class Game():
+  def __init__(self):
+    pg.init()
+    pg.mixer.init()
+    self.screen = pg.display.set_mode(MODE)
+    pg.display.set_caption(TITLE)
+    self.clock = pg.time.Clock()
 
-pg.init()
-pg.mixer.init()
-screen = pg.display.set_mode(MODE)
-pg.display.set_caption(TITLE)
+    self.running = True
+  
+  def new(self):
+    self.all_sprites = pg.sprite.Group()
+    teste = pg.sprite.Sprite()
+    teste.image = pg.Surface((30, 30))
+    teste.image.fill(WHITE)
+    teste.rect = teste.image.get_rect()
+    teste.rect.center = (100, 100)
+    self.all_sprites.add(teste)
+    self.run()
 
-running = True
+  def run(self):
+    self.playing = True
+    while self.playing:
+      self.events()
+      self.update()
+      self.draw()
 
-while running:
-  for event in pg.event.get():
-    if event.type == pg.QUIT:
-      running = False
+  def events(self):
+    events = pg.event.get()
+    for event in events:
+      if event.type == pg.QUIT:
+        self.playing = not self.playing
+        self.running = not self.running
 
-  screen.fill(BLACK)
-  pg.display.flip()
+  def update(self):
+    pass
+  
+  def draw(self):
+    self.screen.fill(BLACK)
+    self.all_sprites.draw(self.screen)
+    pg.display.flip()
+
+g = Game()
+while g.running:
+  g.new()
