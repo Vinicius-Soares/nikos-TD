4b2e35b6a39c6628f4d2cf2a7ece92adad02b62a
Victor Yan Pereira e Lima <victor7095@pop-os.localdomain>
2019-11-15 23:54:33 -0400

    Classes de mobs

--- core/mobs.py
+++ core/mobs.py
@@ -1,1 +1,51 @@
 # Inimigos do jogo
+import pygame as pg
+from .utils import load_image, MINION_SPRITE, RUNNER_SPRITE, FATMAN_SPRITE
+
+class _Mob(pg.sprite.Sprite):
+    def __init__(self, image_path, damage, speed, attack_rate, angle, x_cor, y_cor):
+        pg.sprite.Sprite.__init__(self)
+        self.image, self.rect = load_image(image_path, -1)
+        self.damage = damage
+        self.speed = speed
+        self.attack_rate = attack_rate
+        self.x_cor = x_cor
+        self.y_cor = y_cor
+        self.angle = angle
+        self.rect.center = (x_cor, y_cor)
+
+    def update(self):
+        pass
+    
+    def fire(self):
+        pass
+
+class Minion(_Mob):
+    def __init__(self, x_cor, y_cor):
+        super().__init__(MINION_SPRITE, 1,1,1, x_cor, y_cor)
+
+    def update(self):
+        pass
+    
+    def fire(self):
+        pass
+
+class Runner(_Mob):
+    def __init__(self, x_cor, y_cor):
+        super().__init__(RUNNER_SPRITE, 1,1,1, x_cor, y_cor)
+
+    def update(self):
+        pass
+
+    def fire(self):
+        pass
+
+class Fatman(_Mob):
+    def __init__(self, x_cor, y_cor):
+        super().__init__(FATMAN_SPRITE, 1,1,1, x_cor, y_cor)
+
+    def update(self):
+        pass
+
+    def fire(self):
+        pass
--- core/utils.py
+++ core/utils.py
@@ -1,4 +1,7 @@
+import os
 from pathlib import Path
+from pygame.image import load
+from pygame import error, RLEACCEL
 
 BASE_PATH = Path()
 FONT = BASE_PATH / 'assets' / 'fonts' / 'Pixeled.ttf'
@@ -7,6 +10,10 @@ WIDTH = 1024
 HEIGHT = 768
 MODE = (WIDTH, HEIGHT)
 TITLE = "Niko's Tower Defense"
+
+MINION_SPRITE = BASE_PATH / 'assets' / 'sprites' / 'minion.html'
+RUNNER_SPRITE = BASE_PATH / 'assets' / 'sprites' / 'runner.html'
+FATMAN_SPRITE = BASE_PATH / 'assets' / 'sprites' / 'fatman.html'
 
 COLORS = {
     'black': (0, 0, 0),
@@ -19,3 +26,18 @@ COLORS = {
     'begin': (0, 255, 0),
     'end': (255, 0, 0),
 }
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('assets', name)
+    try:
+        image = load(fullname)
+    except error as message:
+        print('Cannot load image:', name)
+        raise SystemExit(message)
+    image = image.convert()
+    if colorkey is not None:
+        if colorkey is -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey, RLEACCEL)
+    return image, image.get_rect()
