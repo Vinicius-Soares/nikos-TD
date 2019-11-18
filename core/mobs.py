fc491edc2a7e2abc82f7e34e79500099aa5fb622
Victor Yan Pereira e Lima <victor7095@pop-os.localdomain>
2019-11-17 23:35:28 -0400

    Merge movimentação

--- core/mobs.py
+++ core/mobs.py
@@ -3,49 +3,72 @@ import pygame as pg
 from .utils import load_image, MINION_SPRITE, RUNNER_SPRITE, FATMAN_SPRITE
 
 class _Mob(pg.sprite.Sprite):
-    def __init__(self, image_path, damage, speed, attack_rate, x_cor, y_cor):
+    def __init__(self, image_path, damage, speed, attack_rate, cors, path):
         pg.sprite.Sprite.__init__(self)
         self.image, self.rect = load_image(image_path, -1)
         self.damage = damage
         self.speed = speed
         self.attack_rate = attack_rate
-        self.x_cor = x_cor
-        self.y_cor = y_cor
-        # self.angle = angle
-        self.rect.center = (x_cor, y_cor)
+        self.x_cor, self.y_cor = cors
+        self.path = path
+        self.rect.center = cors
+        self._next_block_index = 0
 
     def update(self):
-        pass
+        reached_destiny = self._next_block_index == len(self.path)
+
+        if not reached_destiny:
+            next_x, next_y = self.path[self._next_block_index]
+
+            if self.x_cor == next_x and self.y_cor == next_y:
+                if self._next_block_index < len(self.path) - 1:
+                    self._next_block_index+=1
+                block_x, block_y = self.path[self._next_block_index]
+
+                current_block_index = self._next_block_index-1
+                current_block_x, current_block_y = self.path[current_block_index]
+
+                self._dx = (block_x - current_block_x)/(50-self.speed)
+                self._dy = (block_y - current_block_y)/(50-self.speed)
+
+            else:
+                self.x_cor+=int(self._dx)
+                self.y_cor+=int(self._dy)
+                self.rect.center = (self.x_cor, self.y_cor)
+    
     
     def fire(self):
         pass
 
 class Minion(_Mob):
-    def __init__(self, x_cor, y_cor):
-        super().__init__(MINION_SPRITE, 1,1,1, x_cor, y_cor)
+    def __init__(self, cors, path):
+        self._next_block_index = 0
+        super().__init__(MINION_SPRITE, 1,1,1, cors, path)
 
     def update(self):
-        pass
+        super().update()
     
     def fire(self):
         pass
 
 class Runner(_Mob):
-    def __init__(self, x_cor, y_cor):
+    def __init__(self, cors):
+        x_cor, y_cor = cors
         super().__init__(RUNNER_SPRITE, 1,1,1, x_cor, y_cor)
 
     def update(self):
-        pass
+        super().update()
 
     def fire(self):
         pass
 
 class Fatman(_Mob):
-    def __init__(self, x_cor, y_cor):
+    def __init__(self, cors):
+        x_cor, y_cor = cors
         super().__init__(FATMAN_SPRITE, 1,1,1, x_cor, y_cor)
 
     def update(self):
-        pass
-
+        super().update()
+        
     def fire(self):
         pass
