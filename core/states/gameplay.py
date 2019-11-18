b4f53e90d245284d51f6d834db3a7d1144f4af7e
Victor Yan Pereira e Lima <victor7095@pop-os.localdomain>
2019-11-17 23:55:35 -0400

    Teste movimentação

--- core/states/gameplay.py
+++ core/states/gameplay.py
@@ -15,11 +15,21 @@ class Gameplay(state_machine._State):
         state_machine._State.startup(self, now, persistant)
         self.all_sprites = pg.sprite.Group()
 
+        full_path = ((100, 100),
+                     (100, 200),
+                     (200, 200),
+                     (200, 300),
+                     (300, 300),
+                     (300, 400),
+                     (400, 400),
+                     (400, 500),
+                     (500, 500))
+
         begin = pg.sprite.Sprite()
         begin.image = pg.Surface((100, 100))
         begin.image.fill(COLORS['begin'])
         begin.rect = begin.image.get_rect()
-        begin.rect.center = (100, HEIGHT / 2)
+        begin.rect.center = full_path[0]
         self.all_sprites.add(begin)
 
         for i in range(8):
@@ -27,34 +37,34 @@ class Gameplay(state_machine._State):
             path.image = pg.Surface((100, 100))
             path.image.fill(COLORS['path'])
             path.rect = path.image.get_rect()
-            path.rect.center = (190 + (i * 100), HEIGHT / 2)
+            path.rect.center = full_path[i+1]
             self.all_sprites.add(path)
 
         end = pg.sprite.Sprite()
         end.image = pg.Surface((100, 100))
         end.image.fill(COLORS['end'])
         end.rect = end.image.get_rect()
-        end.rect.center = (900, HEIGHT / 2)
+        end.rect.center = full_path[-1]
         self.all_sprites.add(end)
 
         base = pg.sprite.Sprite()
         base.image = pg.Surface((100, 100))
         base.image.fill(COLORS['base'])
         base.rect = base.image.get_rect()
-        base.rect.center = (WIDTH / 2, 500)
+        base.rect.center = (WIDTH / 2, 700)
         self.all_sprites.add(base)
 
-        turret = Turret(WIDTH / 2, 500)
+        turret = Turret(WIDTH / 2, 300)
         self.all_sprites.add(turret)
 
-        minion = Minion(100, 100)
+        minion = Minion(full_path[0], full_path)
         self.all_sprites.add(minion)
 
     def get_event(self, event):
         pass
 
     def update(self, keys, now):
-        pass
+        self.all_sprites.update()
 
     def draw(self, surface):
         surface.fill((0, 0, 0))
