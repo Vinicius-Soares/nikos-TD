96798268da19abd88b6a27bf5464edf7ccebbfdc
Victor Yan Pereira e Lima <vypl.snf19@uea.edu.br>
2019-11-20 09:59:14 -0400

    Bala matando mob

--- core/components/bullet.py
+++ core/components/bullet.py
@@ -4,19 +4,25 @@ from ..utils import load_image, BULLET_SPRITE
 class Bullet(pg.sprite.Sprite):
     def __init__(self, cors, target, speed):
         super().__init__()
-        self.image, self.rect = load_image(BULLET_SPRITE, -1)
+        self.image = pg.transform.scale(load_image(BULLET_SPRITE, -1)[0], (50,50))
+        self.rect = self.image.get_rect()
+        self.rect.center = cors
         self.origin = cors
         self.x_cor, self.y_cor = cors
         self.target = target
         self.speed= speed
+        self.damage = 2
 
     def update(self):
-        self._dx = ((self.target.x_cor-self.x_cor)*self.speed)/200
-        self._dy = ((self.target.y_cor-self.y_cor)*self.speed)/200
+        self._dx = ((self.target.x_cor-self.x_cor)*self.speed)/10
+        self._dy = ((self.target.y_cor-self.y_cor)*self.speed)/10
 
         self.x_cor+=self._dx
         self.y_cor+=self._dy
 
         self.rect.center = (self.x_cor, self.y_cor)
         hit_target = self.rect.colliderect(self.target.rect)
-        if hit_target: self.kill()
+        if hit_target:
+            self.target.health-=self.damage
+            if self.target.health == 0: self.target.kill() 
+            self.kill()
