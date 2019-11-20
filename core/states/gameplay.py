3a8a290f8c7749dc97be0fa3b0fb15244654c036
Victor Yan Pereira e Lima <vypl.snf19@uea.edu.br>
2019-11-20 10:00:33 -0400

    Gerando infifnitos mobs para testes

--- core/states/gameplay.py
+++ core/states/gameplay.py
@@ -12,39 +12,38 @@ class Gameplay(state_machine._State):
         self.next = "MENU"
         self.bullets = pg.sprite.Group()
         self.all_sprites = pg.sprite.Group()
+        self.mobs = pg.sprite.Group()
+        self.full_path = ((100, 100),
+                          (100, 200),
+                          (200, 200),
+                          (200, 300),
+                          (300, 300),
+                          (300, 400),
+                          (400, 400),
+                          (400, 500),
+                          (500, 500))[::-1]
+        self.i = 0
 
     def startup(self, now, persistant):
         state_machine._State.startup(self, now, persistant)
 
-        full_path = ((100, 100),
-                     (100, 200),
-                     (200, 200),
-                     (200, 300),
-                     (300, 300),
-                     (300, 400),
-                     (400, 400),
-                     (400, 500),
-                     (500, 500))
-
-        full_path = full_path[::-1]
-
         begin = pg.sprite.Sprite()
         begin.image = pg.transform.scale(load_image(BEGIN_SPRITE, -1)[0], (100,100))
         begin.rect = begin.image.get_rect()
-        begin.rect.center = full_path[0]
+        begin.rect.center = self.full_path[0]
         self.all_sprites.add(begin)
 
         for i in range(7):
             path = pg.sprite.Sprite()
             path.image = pg.transform.scale(load_image(ENEMYPATH_SPRITE, -1)[0], (100,100))
             path.rect = path.image.get_rect()
-            path.rect.center = full_path[i+1]
+            path.rect.center = self.full_path[i+1]
             self.all_sprites.add(path)
 
         end = pg.sprite.Sprite()
         end.image = pg.transform.scale(load_image(END_SPRITE, -1)[0], (100,100))
         end.rect = end.image.get_rect()
-        end.rect.center = full_path[-1]
+        end.rect.center = self.full_path[-1]
         self.all_sprites.add(end)
 
         base = pg.sprite.Sprite()
@@ -53,22 +52,23 @@ class Gameplay(state_machine._State):
         base.rect.center = (WIDTH / 2, 300)
         self.all_sprites.add(base)
 
-        minion = mobs.Minion(full_path[0], full_path)
-        self.all_sprites.add(minion)
-
-        mob_list = [minion]
-
-        turret = towers.Turret((WIDTH / 2, 300), mob_list, self.bullets)
+        turret = towers.Turret((WIDTH / 2, 300), self.mobs, self.bullets)
         self.all_sprites.add(turret)
 
     def get_event(self, event):
         pass
 
     def update(self, keys, now):
+        if(self.i % 150 == 0):
+            minion = mobs.Minion(self.full_path[0], self.full_path)
+            self.mobs.add(minion)
         self.all_sprites.update()
+        self.mobs.update()
         self.bullets.update()
+        self.i+=1
 
     def draw(self, surface):
         surface.fill((0, 0, 0))
         self.all_sprites.draw(surface)
+        self.mobs.draw(surface)
         self.bullets.draw(surface)
